d24a510bea6555e8b58ff92a652fa98d8eb8aab7
John Ric Merque <johnricmer@gmail.com>
2024-07-30 04:06:14 +0800

    setup tailwindcss

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>React</title>
 </head>
+@vite('resources/css/app.css')
 <body>
     <div id="root"></div>
     @viteReactRefresh
